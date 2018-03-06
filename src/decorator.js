bde9fad46902b1c19d0d0ee6424535952ddcbb95
Aubacca <aubacca.renhor@gmail.com>
2018-03-06 08:12:40 +0100

    Decorator pattern with samples implemented.

--- src/decorator.js
+++ src/decorator.js
@@ -1,1 +1,147 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+console.log("*****************************");
 console.log("*** The Decorator Pattern ***");
+console.log("*****************************");
+/**
+ * Abstract bicycle class. Must be implemented by all bicyles.
+ */
+var Bicycle = /** @class */ (function () {
+    function Bicycle() {
+    }
+    Bicycle.prototype.getDescription = function () {
+        return this.description;
+    };
+    return Bicycle;
+}());
+/**
+ * Sample bicycle Wheeler DX.
+ */
+var WheelerDX = /** @class */ (function (_super) {
+    __extends(WheelerDX, _super);
+    function WheelerDX() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.description = "Wheeler DX components (500)";
+        return _this;
+    }
+    WheelerDX.prototype.getCost = function () {
+        return 500;
+    };
+    return WheelerDX;
+}(Bicycle));
+/**
+ * Sample bicycle Wheeler TX.
+ */
+var WheelerTX = /** @class */ (function (_super) {
+    __extends(WheelerTX, _super);
+    function WheelerTX() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.description = "Wheeler TX components (800)";
+        return _this;
+    }
+    WheelerTX.prototype.getCost = function () {
+        return 800;
+    };
+    return WheelerTX;
+}(Bicycle));
+/**
+ * Abstract decorator class. Must be implemented by all bicycle options classes.
+ */
+var BicycleOptionDecorator = /** @class */ (function (_super) {
+    __extends(BicycleOptionDecorator, _super);
+    function BicycleOptionDecorator() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.description = "";
+        return _this;
+    }
+    return BicycleOptionDecorator;
+}(Bicycle));
+/**
+ * Additional color options for a bicycle.
+ */
+var BicycleColorOptions = /** @class */ (function (_super) {
+    __extends(BicycleColorOptions, _super);
+    function BicycleColorOptions(newDecoratedBicycle) {
+        var _this = _super.call(this) || this;
+        _this.newDecoratedBicycle = newDecoratedBicycle;
+        _this.decoratedBicycle = newDecoratedBicycle;
+        return _this;
+    }
+    BicycleColorOptions.prototype.getDescription = function () {
+        return (this.decoratedBicycle.getDescription() +
+            ", Bicolor perlmut green blueish (120)");
+    };
+    BicycleColorOptions.prototype.getCost = function () {
+        return this.decoratedBicycle.getCost() + 120;
+    };
+    return BicycleColorOptions;
+}(BicycleOptionDecorator));
+/**
+ * Additional gear box options for a bicycle.
+ */
+var BicycleGearboxOption = /** @class */ (function (_super) {
+    __extends(BicycleGearboxOption, _super);
+    function BicycleGearboxOption(newDecoratedBicycle) {
+        var _this = _super.call(this) || this;
+        _this.newDecoratedBicycle = newDecoratedBicycle;
+        _this.decoratedBicycle = newDecoratedBicycle;
+        return _this;
+    }
+    BicycleGearboxOption.prototype.getDescription = function () {
+        return (this.decoratedBicycle.getDescription() + ", hydraulic gear box (240)");
+    };
+    BicycleGearboxOption.prototype.getCost = function () {
+        return this.decoratedBicycle.getCost() + 240;
+    };
+    return BicycleGearboxOption;
+}(BicycleOptionDecorator));
+/**
+ * Additional break system options for a bicycle.
+ */
+var BicycleBreakOption = /** @class */ (function (_super) {
+    __extends(BicycleBreakOption, _super);
+    function BicycleBreakOption(newDecoratedBicycle) {
+        var _this = _super.call(this) || this;
+        _this.newDecoratedBicycle = newDecoratedBicycle;
+        _this.decoratedBicycle = newDecoratedBicycle;
+        return _this;
+    }
+    BicycleBreakOption.prototype.getDescription = function () {
+        return this.decoratedBicycle.getDescription() + ", hydraulic breaks (170)";
+    };
+    BicycleBreakOption.prototype.getCost = function () {
+        return this.decoratedBicycle.getCost() + 170;
+    };
+    return BicycleBreakOption;
+}(BicycleOptionDecorator));
+//
+// Test cases for decorators.
+var myDailyBicyle = new WheelerDX();
+var myWeekendBicyle = new WheelerTX();
+console.log("\nMy daily bicycle w/o options:");
+console.log("* Description: " + myDailyBicyle.getDescription());
+console.log("* Cost: " + myDailyBicyle.getCost());
+console.log("My weekend bicycle w/o options:");
+console.log("* Description: " + myWeekendBicyle.getDescription());
+console.log("* Cost: " + myWeekendBicyle.getCost());
+//
+// Adding options to the bicycle via decorators.
+myDailyBicyle = new BicycleColorOptions(myDailyBicyle);
+myDailyBicyle = new BicycleBreakOption(myDailyBicyle);
+myWeekendBicyle = new BicycleGearboxOption(myWeekendBicyle);
+//
+// Show description and costs with bicycle options.
+console.log("\nMy daily bicycle w/ options:");
+console.log("* Description: " + myDailyBicyle.getDescription());
+console.log("* Cost: " + myDailyBicyle.getCost());
+console.log("My weekend bicycle w/ options:");
+console.log("* Description: " + myWeekendBicyle.getDescription());
+console.log("* Cost: " + myWeekendBicyle.getCost());
--- src/decorator.ts
+++ src/decorator.ts
@@ -1,1 +1,134 @@
+console.log("*****************************");
 console.log("*** The Decorator Pattern ***");
+console.log("*****************************");
+
+/**
+ * Abstract bicycle class. Must be implemented by all bicyles.
+ */
+abstract class Bicycle {
+  public abstract description: string;
+
+  public getDescription(): string {
+    return this.description;
+  }
+
+  public abstract getCost(): number;
+}
+
+/**
+ * Sample bicycle Wheeler DX.
+ */
+class WheelerDX extends Bicycle {
+  public description = "Wheeler DX components (500)";
+
+  public getCost(): number {
+    return 500;
+  }
+}
+
+/**
+ * Sample bicycle Wheeler TX.
+ */
+class WheelerTX extends Bicycle {
+  public description = "Wheeler TX components (800)";
+
+  public getCost(): number {
+    return 800;
+  }
+}
+
+/**
+ * Abstract decorator class. Must be implemented by all bicycle options classes.
+ */
+abstract class BicycleOptionDecorator extends Bicycle {
+  abstract decoratedBicycle: Bicycle;
+  public description: string = "";
+  public abstract getDescription(): string;
+  public abstract getCost(): number;
+}
+
+/**
+ * Additional color options for a bicycle.
+ */
+class BicycleColorOptions extends BicycleOptionDecorator {
+  decoratedBicycle: Bicycle;
+
+  constructor(public newDecoratedBicycle: Bicycle) {
+    super();
+    this.decoratedBicycle = newDecoratedBicycle;
+  }
+
+  public getDescription(): string {
+    return (
+      this.decoratedBicycle.getDescription() +
+      ", Bicolor perlmut green blueish (120)"
+    );
+  }
+  public getCost(): number {
+    return this.decoratedBicycle.getCost() + 120;
+  }
+}
+
+/**
+ * Additional gear box options for a bicycle.
+ */
+class BicycleGearboxOption extends BicycleOptionDecorator {
+  decoratedBicycle: Bicycle;
+
+  constructor(public newDecoratedBicycle: Bicycle) {
+    super();
+    this.decoratedBicycle = newDecoratedBicycle;
+  }
+
+  public getDescription(): string {
+    return (
+      this.decoratedBicycle.getDescription() + ", hydraulic gear box (240)"
+    );
+  }
+  public getCost(): number {
+    return this.decoratedBicycle.getCost() + 240;
+  }
+}
+
+/**
+ * Additional break system options for a bicycle.
+ */
+class BicycleBreakOption extends BicycleOptionDecorator {
+  decoratedBicycle: Bicycle;
+
+  constructor(public newDecoratedBicycle: Bicycle) {
+    super();
+    this.decoratedBicycle = newDecoratedBicycle;
+  }
+
+  public getDescription(): string {
+    return this.decoratedBicycle.getDescription() + ", hydraulic breaks (170)";
+  }
+  public getCost(): number {
+    return this.decoratedBicycle.getCost() + 170;
+  }
+}
+//
+// Test cases for decorators.
+let myDailyBicyle = new WheelerDX();
+let myWeekendBicyle = new WheelerTX();
+console.log("\nMy daily bicycle w/o options:");
+console.log(`* Description: ${myDailyBicyle.getDescription()}`);
+console.log(`* Cost: ${myDailyBicyle.getCost()}`);
+console.log("My weekend bicycle w/o options:");
+console.log(`* Description: ${myWeekendBicyle.getDescription()}`);
+console.log(`* Cost: ${myWeekendBicyle.getCost()}`);
+
+//
+// Adding options to the bicycle via decorators.
+myDailyBicyle = new BicycleColorOptions(myDailyBicyle);
+myDailyBicyle = new BicycleBreakOption(myDailyBicyle);
+myWeekendBicyle = new BicycleGearboxOption(myWeekendBicyle);
+//
+// Show description and costs with bicycle options.
+console.log("\nMy daily bicycle w/ options:");
+console.log(`* Description: ${myDailyBicyle.getDescription()}`);
+console.log(`* Cost: ${myDailyBicyle.getCost()}`);
+console.log("My weekend bicycle w/ options:");
+console.log(`* Description: ${myWeekendBicyle.getDescription()}`);
+console.log(`* Cost: ${myWeekendBicyle.getCost()}`);
